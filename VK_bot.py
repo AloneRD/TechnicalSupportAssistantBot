596d5b6533d6832b8a295ebf4e418f8e7ec47b37
Artur <artur_goncharov_97@mail.ru>
2022-06-25 21:19:02 +0300

    moved vk token  to env

--- VK_bot.py
+++ VK_bot.py
@@ -1,15 +1,25 @@
 import vk_api
+import os
+from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-vk_session = vk_api.VkApi(token="vk1.a.BxE87b-9oWfUBcdw2eLN4TFz_6UQ7BoIocp-KqptmCmYigE4TWOb4GMSI9ub-Psytdw6G_xXL0QB8LAEh2I1hDmEcjc9it9cwnyWw7Rz-G_V5NS1-19hRUubDiRZPeeROHa8tHvOX2uYsZAiS2gtvODaS6Py5LeOs5MwnONK2mBDgOSjdUuqV30QiPCjfhLq")
 
-longpoll = VkLongPoll(vk_session)
+def main():
+    load_dotenv()
+    vk_token = os.getenv("VK_TOKEN")
+    vk_session = vk_api.VkApi(token=vk_token)
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        print('Новое сообщение:')
-        if event.to_me:
-            print('Для меня от: ', event.user_id)
-        else:
-            print('От меня для: ', event.user_id)
-        print('Текст:', event.text)
+    longpoll = VkLongPoll(vk_session)
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            print('Новое сообщение:')
+            if event.to_me:
+                print('Для меня от: ', event.user_id)
+            else:
+                print('От меня для: ', event.user_id)
+            print('Текст:', event.text)
+
+
+if __name__ == '__main__':
+    main()
