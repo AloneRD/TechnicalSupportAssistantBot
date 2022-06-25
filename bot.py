2ea9c18e74f192237f88e36c7eba4cc080b104f8
Artur <artur_goncharov_97@mail.ru>
2022-06-25 23:11:17 +0300

    add Telegram Logger

--- bot.py
+++ bot.py
@@ -6,6 +6,22 @@ from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler
 from telegram.ext import Filters
 from google.cloud import dialogflow
+import telegram
+import logging
+
+logger = logging.getLogger("CheckBot")
+
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, token, chat_id) -> None:
+        super().__init__()
+        self.bot = telegram.Bot(token)
+        self.chat_id = chat_id
+
+    def emit(self, record) -> None:
+        log_entry = self.format(record)
+        self.bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def start_callback(update, context) -> NoReturn:
@@ -20,19 +36,26 @@ def echo(update, context) -> NoReturn:
 
 
 def detect_intent_texts(project_id: str, session_id: str, text) -> str:
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(project_id, session_id)
-    text_input = dialogflow.TextInput(text=text, language_code="ru")
-    query_input = dialogflow.QueryInput(text=text_input)
-    response = session_client.detect_intent(
-            request={"session": session, "query_input": query_input}
-        )
-    return response.query_result.fulfillment_text
+    try:
+        session_client = dialogflow.SessionsClient()
+        session = session_client.session_path(project_id, session_id)
+        text_input = dialogflow.TextInput(text=text, language_code="ru")
+        query_input = dialogflow.QueryInput(text=text_input)
+        response = session_client.detect_intent(
+                request={"session": session, "query_input": query_input}
+            )
+        return response.query_result.fulfillment_text  
+    except:
+        logger.exception("Бот упал с ошибкой")
 
 
 def main():
     load_dotenv()
     tg_token = os.getenv("TELEGRAM_TOKEN")
+    chat_id = os.environ.get("TG_CHAT_ID")
+
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramLogsHandler(tg_token, chat_id))
 
     updater = Updater(token=tg_token)
     dispacher = updater.dispatcher
