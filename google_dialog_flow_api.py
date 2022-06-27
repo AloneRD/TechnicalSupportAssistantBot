2b83a3c4b3e068806858a701270dc0a659595bab
Artur <artur_goncharov_97@mail.ru>
2022-06-27 22:55:13 +0300

    changed type hint for function detect_intent_texts

--- google_dialog_flow_api.py
+++ google_dialog_flow_api.py
@@ -1,7 +1,9 @@
+from typing import Union
+from typing import NoReturn
 from google.cloud import dialogflow
 
 
-def detect_intent_texts(project_id: str, session_id: str, text, type_bot: str) -> str:
+def detect_intent_texts(project_id: str, session_id: str, text, type_bot: str) -> Union[str, NoReturn]:
     session_client = dialogflow.SessionsClient()
     session = session_client.session_path(project_id, session_id)
     text_input = dialogflow.TextInput(text=text, language_code="ru")
