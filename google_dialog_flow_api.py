85dfa39da4b6df08960b01d89cb523e8ea008ce8
Artur <artur_goncharov_97@mail.ru>
2022-06-28 22:10:10 +0300

    deleted argument  type_bot of function detect_intent_texts

--- google_dialog_flow_api.py
+++ google_dialog_flow_api.py
@@ -3,7 +3,7 @@ from typing import NoReturn
 from google.cloud import dialogflow
 
 
-def detect_intent_texts(project_id: str, session_id: str, text, type_bot: str) -> Union[str, NoReturn]:
+def detect_intent_texts(project_id: str, session_id: str, text) -> Union[str, NoReturn]:
     session_client = dialogflow.SessionsClient()
     session = session_client.session_path(project_id, session_id)
     text_input = dialogflow.TextInput(text=text, language_code="ru")
@@ -11,8 +11,4 @@ def detect_intent_texts(project_id: str, session_id: str, text, type_bot: str) -
     response = session_client.detect_intent(
             request={"session": session, "query_input": query_input}
         )
-    if type_bot == "vk":
-        if not response.query_result.intent.is_fallback:
-            return response.query_result.fulfillment_text
-    else:
-        return response.query_result.fulfillment_text
+    return response.query_result.fulfillment_text, response.query_result.intent.is_fallback
